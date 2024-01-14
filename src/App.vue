919db1cbc3d79ed49da9ed6323ea6d2448fd6008
lvdengming <lvdengming@foxmail.com>
2024-01-14 21:59:48 +0800

    feat: 修改 prettier 分号配置

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView } from 'vue-router';
 import SideMenu from './views/side-menu/SideMenu.vue';
 </script>
 
 <template>
-    <SideMenu/>
+    <SideMenu />
     <RouterView />
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
 
-const routes: Array<RouteRecordRaw> = []
+const routes: Array<RouteRecordRaw> = [];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes
-})
+});
 
-export default router
+export default router;
