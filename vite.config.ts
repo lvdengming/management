64fac4cb5773610a0ac87d3dc65410fced7c39f8
lvdengming <lvdengming@foxmail.com>
2024-01-14 21:52:54 +0800

    feat: 添加路径配置

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+            '@router': fileURLToPath(new URL('./src/router', import.meta.url))
         }
     }
 })
